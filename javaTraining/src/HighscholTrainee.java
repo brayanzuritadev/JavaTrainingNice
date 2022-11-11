55671b2d17fe272043558eb22bf81f2697850e24
Brayan <brayanzuritavargas@gmail.com>
2022-11-11 11:23:51 -0400

    Data Types and variables in Java

---
+++ javaTraining/src/HighscholTrainee.java
@@ -0,0 +1,16 @@
+public class HighscholTrainee {
+    public static void main(String[] args){
+
+        int studentAge = 17;
+        double prom = 7.5;
+        char firstInitial = 'B';
+        char lastInitial = 'Z';
+        boolean attendance = true;
+
+        System.out.println("The student Age is: " + studentAge);
+        System.out.println("The prom is: " + prom);
+        System.out.println("The First Initial is: " + firstInitial);
+        System.out.println("The Last Initial is: " + lastInitial);
+        System.out.println("The attendance is: " + attendance);
+    }
+}
