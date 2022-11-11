7441745c25bbd8d41b3004e286e35c711bd27edb
Brayan <brayanzuritavargas@gmail.com>
2022-11-11 11:41:58 -0400

    Data Types and variables in Java

--- javaTraining/src/HighscholTrainee.java
+++ javaTraining/src/HighscholTrainee.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class HighscholTrainee {
     public static void main(String[] args){
 
@@ -7,10 +8,29 @@ public class HighscholTrainee {
         char lastInitial = 'Z';
         boolean attendance = true;
 
+        String studentName = "Carlos Andres";
+        String firstname = "Carlos";
+        String lastName = "Azcarraga";
+
         System.out.println("The student Age is: " + studentAge);
         System.out.println("The prom is: " + prom);
         System.out.println("The First Initial is: " + firstInitial);
         System.out.println("The Last Initial is: " + lastInitial);
         System.out.println("The attendance is: " + attendance);
+        System.out.println("The student Name is: " + studentName);
+        System.out.println("The student First Name is: " + firstname);
+        System.out.println("The student Last Name is: " + lastName);
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Desea cambiar su apellido y promedio?");
+        String respuesta =entrada.nextLine();
+        if(respuesta.toUpperCase().equals("SI")){
+            System.out.println("Ingrese su promedio:");
+            prom=Integer.parseInt(entrada.nextLine());
+            System.out.println("Ingrese su Apellido:");
+            lastName=entrada.nextLine();
+        }
+        System.out.println("The prom is: " + prom);
+        System.out.println("The student Last Name is: " + lastName);
     }
 }
